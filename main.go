cc9f9dc2004d0b68a6b59d636eebbad46416d897
Vitaliy Kondratiev <kondratiev.vitaliy2012@yandex.ru>
2021-11-15 17:05:00 +0500

    List command now prints all Jira instances

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"./src/helpers"
 	"./src/jira"
 	"./src/toggl"
+
+	"github.com/fatih/color"
 )
 
 type DisplayIssue struct {
@@ -81,116 +83,125 @@ func CommandAuth() {
 func CommandList() {
 	gconfig := (config.GlobalConfig{}).LoadConfig(true)
 	_toggl := toggl.Toggl{Config: gconfig.Toggl}
-	var _jira jira.Jira
-	if len(gconfig.Jira) > 1 {
-		jiraIndex := gconfig.SelectJiraInstance()
-		_jira = jira.Jira{Config: gconfig.Jira[jiraIndex]}
-	} else {
-		_jira = jira.Jira{Config: gconfig.Jira[0]}
-	}
 	togglUsername, _ := _toggl.GetUser()
 	const padding = 3
 	writer := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', tabwriter.StripEscape)
+	blue := color.New(color.FgHiBlue).SprintFunc()
+	cyan := color.New(color.FgHiCyan).SprintFunc()
+	yellow := color.New(color.FgHiYellow).SprintFunc()
+	bold := color.New(color.Bold).SprintFunc()
 	fmt.Fprintf(writer,
 		"\t%v:\t%v\t\n",
-		helpers.GetFormattedDomain("toggl.com"),
+		blue(helpers.GetFormattedDomain("toggl.com")),
 		*togglUsername,
 	)
-	fmt.Fprintf(writer,
-		"\t%v:\t%v\t\n",
-		helpers.GetFormattedDomain(_jira.Config.Domain),
-		_jira.Config.Username,
-	)
-	writer.Flush()
-	fmt.Println()
-	displayIssues := make(map[string]DisplayIssue)
-	for _, issue := range _jira.GetAssignedIssues() {
-		displayIssues[issue.Key] = DisplayIssue{
-			Key:            issue.Key,
-			Summary:        issue.Summary,
-			TrackingStatus: "",
-			UncommitedTime: 0,
-		}
-	}
-	totalUncommitedTime := 0
-	var notAssignedKeys []string
-	for _, _timeEntry := range _toggl.GetTimeEntries() {
-		if !_timeEntry.IsUncommitedEntry() || !_timeEntry.IsJiraDomainEntry(_jira.Config.Domain) {
-			continue
-		}
-		uncommitedTime := _timeEntry.GetDuration()
-		totalUncommitedTime += uncommitedTime
-		if val, ok := displayIssues[_timeEntry.Description]; ok {
-			val.UncommitedTime += uncommitedTime
-			if len(val.TrackingStatus) == 0 {
-				val.TrackingStatus = "uncommited"
-			}
-			if _timeEntry.IsCurrent() {
-				val.TrackingStatus = "current"
-			}
-			displayIssues[_timeEntry.Description] = val
-		} else {
-			trackingStatus := "uncommited"
-			if _timeEntry.IsCurrent() {
-				trackingStatus = "current"
-			}
-			displayIssues[_timeEntry.Description] = DisplayIssue{
-				Key:            _timeEntry.Description,
-				Summary:        "",
-				TrackingStatus: fmt.Sprintf("%s (not assigned)", trackingStatus),
-				UncommitedTime: uncommitedTime,
-			}
-			notAssignedKeys = append(notAssignedKeys, _timeEntry.Description)
-		}
-	}
-	if len(notAssignedKeys) > 0 {
-		for _, unassignedIssue := range _jira.GetIssuesByField(notAssignedKeys, "key") {
-			displayIssue := displayIssues[unassignedIssue.Key]
-			displayIssue.Summary = unassignedIssue.Summary
-			displayIssues[unassignedIssue.Key] = displayIssue
-		}
-	}
-	if len(displayIssues) > 0 {
-		keys := make([]string, 0, len(displayIssues))
-		for key := range displayIssues {
-			keys = append(keys, key)
-		}
-		sort.Strings(keys)
-
+	for jiraConfigIndex, jiraConfig := range gconfig.Jira {
+		var _jira = jira.Jira{Config: jiraConfig}
 		fmt.Fprintf(writer,
-			"\t%v\t%v\t%v\t%v\t\n",
-			"Issue key",
-			"Issue summary",
-			"Time",
-			"Tracking status",
+			"\t%v: %v",
+			cyan(helpers.GetFormattedDomain(_jira.Config.Domain)),
+			_jira.Config.Username,
 		)
-		fmt.Fprintf(writer,
-			"\t%v\t%v\t%v\t%v\t\n",
-			"--------",
-			"-------------",
-			"----",
-			"---------------",
-		)
-		for _, key := range keys {
-			displayIssue := displayIssues[key]
-			fmt.Fprintf(writer,
-				"\t%v\t%.30v...\t%v\t%v\t\n",
-				displayIssue.Key,
-				displayIssue.Summary,
-				helpers.FormatTimeFromUnix(displayIssue.UncommitedTime),
-				displayIssue.TrackingStatus,
-			)
-		}
 		writer.Flush()
 		fmt.Println()
-		fmt.Fprintf(writer,
-			"\tUncommited time:\t%v\t\n",
-			helpers.FormatTimeFromUnix(totalUncommitedTime),
-		)
-	} else {
-		fmt.Fprintf(writer,
-			"\tYou don't have assigned or uncommited task\n",
-		)
+		displayIssues := make(map[string]DisplayIssue)
+		for _, issue := range _jira.GetAssignedIssues() {
+			displayIssues[issue.Key] = DisplayIssue{
+				Key:            issue.Key,
+				Summary:        issue.Summary,
+				TrackingStatus: "",
+				UncommitedTime: 0,
+			}
+		}
+		totalUncommitedTime := 0
+		var notAssignedKeys []string
+		for _, _timeEntry := range _toggl.GetTimeEntries() {
+			if !_timeEntry.IsUncommitedEntry() || !_timeEntry.IsJiraDomainEntry(_jira.Config.Domain) {
+				continue
+			}
+			uncommitedTime := _timeEntry.GetDuration()
+			totalUncommitedTime += uncommitedTime
+			if val, ok := displayIssues[_timeEntry.Description]; ok {
+				val.UncommitedTime += uncommitedTime
+				if len(val.TrackingStatus) == 0 {
+					val.TrackingStatus = "uncommited"
+				}
+				if _timeEntry.IsCurrent() {
+					val.TrackingStatus = "current"
+				}
+				displayIssues[_timeEntry.Description] = val
+			} else {
+				trackingStatus := "uncommited"
+				if _timeEntry.IsCurrent() {
+					trackingStatus = "current"
+				}
+				displayIssues[_timeEntry.Description] = DisplayIssue{
+					Key:            _timeEntry.Description,
+					Summary:        "",
+					TrackingStatus: fmt.Sprintf("%s (not assigned)", trackingStatus),
+					UncommitedTime: uncommitedTime,
+				}
+				notAssignedKeys = append(notAssignedKeys, _timeEntry.Description)
+			}
+		}
+		if len(notAssignedKeys) > 0 {
+			for _, unassignedIssue := range _jira.GetIssuesByField(notAssignedKeys, "key") {
+				displayIssue := displayIssues[unassignedIssue.Key]
+				displayIssue.Summary = unassignedIssue.Summary
+				displayIssues[unassignedIssue.Key] = displayIssue
+			}
+		}
+		if len(displayIssues) > 0 {
+			keys := make([]string, 0, len(displayIssues))
+			for key := range displayIssues {
+				keys = append(keys, key)
+			}
+			sort.Strings(keys)
+
+			fmt.Fprintf(writer,
+				"\t%v\t%v\t%v\t%v\t\n",
+				"Issue key",
+				"Issue summary",
+				"Time",
+				"Tracking status",
+			)
+			fmt.Fprintf(writer,
+				"\t%v\t%v\t%v\t%v\t\n",
+				"--------",
+				"-------------",
+				"----",
+				"---------------",
+			)
+			for keyIndex, key := range keys {
+				displayIssue := displayIssues[key]
+				fmt.Fprintf(writer,
+					"\t%v\t%.35v...\t%v\t%v",
+					displayIssue.Key,
+					displayIssue.Summary,
+					helpers.FormatTimeFromUnix(displayIssue.UncommitedTime),
+					displayIssue.TrackingStatus,
+				)
+				if keyIndex < len(keys)-1 {
+					fmt.Fprintf(writer, "\t\n")
+				}
+			}
+			writer.Flush()
+			fmt.Println()
+			fmt.Fprintf(writer,
+				"\t%s %s: %v\n",
+				yellow("Uncommited time on"),
+				yellow(_jira.Config.Domain),
+				bold(helpers.FormatTimeFromUnix(totalUncommitedTime)),
+			)
+		} else {
+			fmt.Fprintf(writer,
+				"\tYou don't have assigned or uncommited task on %s\n",
+				_jira.Config.Domain,
+			)
+		}
+		if jiraConfigIndex < len(gconfig.Jira)-1 {
+			fmt.Fprintf(writer, "\t\n")
+		}
 	}
 	writer.Flush()
 }
